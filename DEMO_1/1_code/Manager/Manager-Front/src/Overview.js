8cadfd8612f96834e2ad4fba4aba724c4770d988
Swetha Angara <swetha.angara5689@gmail.com>
2020-04-03 14:10:43 -0400

    cleared

--- DEMO_1/1_code/Manager/Manager-Front/src/Overview.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-/* Garage Overview Page for Manager Access Portal */
-function Overview() {
-    return <p></p>;
-}
-export default Overview;
--- DEMO_1/1_code/Manager/Manager-Front/src/Pricing.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-/* Pricing Page for Manager Access Portal */
-function Pricing() {
-    return <h1>Hello! Welcome to SmarkPark!</h1>;
-}
-export default Pricing;
